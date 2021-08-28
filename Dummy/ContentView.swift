a867f3de7fd430edcf0dd00681e6574b682d6d9e
manijee <inboxtomanijee@gmail.com>
2021-08-28 13:22:04 +0530

    Initial Commit

---
+++ Dummy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dummy
+//
+//  Created by Mani Jee on 28/08/21.
+//  Copyright © 2021 Mani Jee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
